31abaec93711e16b7bf6e6f114fb316b2f223c97
Alessio Napoli <alessionapoli99@icloud.com>
2022-03-29 14:11:01 +0200

    Created Section Description Comic

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -9,5 +9,24 @@
         <span class="target">{{$comic['type']}}</span>
       </div>
     </div>
+    <div class="content">
+      <div class="description">
+        <h1>{{$comic['title']}}</h1>
+        <div class="buy">
+          <div class="price">
+            <span>U.S. Price : {{$comic['price']}}</span>
+            <span class="available">Available</span>
+          </div>
+          <div class="checkAvailability">
+            <span>Check Availability</span>
+          </div>
+        </div>
+        <p>{{$comic['description']}}</p>
+      </div>
+      <div class="adv">
+        <span>Advertisement</span>
+        <img src="{{asset('img/adv.jpg')}}" alt="adv">
+      </div>
+    </div>
   </div>
 @endsection
