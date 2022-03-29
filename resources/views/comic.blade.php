0e79c275c12b824eb9d44277103ea7993e9765af
Alessio Napoli <alessionapoli99@icloud.com>
2022-03-29 15:09:22 +0200

    Created InfoContent

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -28,5 +28,43 @@
         <img src="{{asset('img/adv.jpg')}}" alt="adv">
       </div>
     </div>
+    <div class="infoComic">
+      <div class="content">
+        <div class="talent">
+          <h2>Talent</h2>
+          <div class="art border">
+            <span class="title">Art by:</span>
+            <div class="person">
+              @foreach ($comic['artists'] as $artist)
+                  <span>{{$artist}},</span>
+              @endforeach
+            </div>
+          </div>
+          <div class="written">
+            <span class="title">Written by:</span>
+            <div class="person">
+              @foreach ($comic['writers'] as $writer)
+                  <span>{{$writer}},</span>
+              @endforeach
+            </div>
+          </div>
+        </div>
+        <div class="specs">
+          <h2>Specs</h2>
+          <div class="series border">
+            <span class="title">Series:</span>
+            <span class="data">{{$comic['series']}}</span>
+          </div>
+          <div class="price">
+            <span class="title">U.S. Prices:</span>
+            <span class="data">{{$comic['price']}}</span>
+          </div>
+          <div class="sale">
+            <span class="title">On Sale Date:</span>
+            <span class="data">{{$comic['sale_date']}}</span>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 @endsection
